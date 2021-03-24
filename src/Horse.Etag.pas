f2a47c3ae1e59a8800d78454db5738c456d097d0
Anderson Ramos <anderson.ramlug@gmail.com>
2021-03-24 09:11:29 -0300

    Update Horse.Etag.pas

--- src/Horse.Etag.pas
+++ src/Horse.Etag.pas
@@ -27,7 +27,7 @@ begin
   try
     Next;
   finally
-    LContent := THorseHackResponse(Res).GetContent;
+    LContent := THorseHackResponse(Res).Content;
 
     if Assigned(LContent) and LContent.InheritsFrom({$IF DEFINED(FPC)}TJSONData{$ELSE}TJSONValue{$ENDIF}) then
     begin
@@ -42,7 +42,7 @@ begin
     if (Req.Headers['If-None-Match'] = eTag) and                                                              (eTag <> '') then
       Res.Status(THTTPStatus.NotModified);
 
-    THorseHackResponse(Res).GetWebResponse.SetCustomHeader('ETag', eTag);
+    THorseHackResponse(Res).RawWebResponse.SetCustomHeader('ETag', eTag);
   end;
 end;
 
